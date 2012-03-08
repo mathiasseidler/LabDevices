1e9994cc666031ae655d13cc071dc45da4ffcb15
Mathias S <mseidler.work@gmail.com>
2012-03-08 17:01:38 +0800

    save plot (numpy.save)

--- Experiments/Gui_example.py
+++ Experiments/Gui_example.py
@@ -5,9 +5,11 @@
 
 from enthought.traits.api import HasTraits, Str, Instance, Array, Button, Any, Enum, Int, Event,Trait, Callable
 from enthought.traits.ui.api import View, VGroup, HGroup, Item, Controller, Group
+from enthought.enable.api import BaseTool
 from enthought.traits.api import on_trait_change
 from enthought.traits.ui.editors import ButtonEditor
 from enthought.chaco.default_colormaps import *
+from enthought.traits.ui.menu import Action, CloseAction, Menu, MenuBar, OKCancelButtons, Separator
                  
 from enthought.chaco.api import ArrayDataSource, ArrayPlotData, ColorBar, ContourLinePlot, \
                                  ColormappedScatterPlot, CMapImagePlot, \
@@ -29,7 +31,7 @@ from time import sleep
 from enthought.traits.ui.menu import Action, CloseAction, Menu, \
                                      MenuBar, NoButtons, Separator
 
-from numpy import array, linspace, meshgrid, nanmin, nanmax,  pi, zeros, ones
+from numpy import array, linspace, meshgrid, nanmin, nanmax,  pi, zeros, ones, save
 
 from enthought.chaco.tools.api import LineInspector, PanTool, RangeSelection, \
                                    RangeSelectionOverlay, ZoomTool, DragZoom
@@ -45,211 +47,6 @@ class FieldData(HasTraits):
     intensity_map = Array
     data = Array
     index = Array
-    
-class PlotUI(HasTraits):
-    #Traits view definitions:
-    traits_view = View(
-        Group(Item('container',
-                   editor=ComponentEditor(size=(800,600)),
-                   show_label=False)),
-        buttons=NoButtons,
-        resizable=True)
-    plot_edit_view = View(
-        Group(Item('num_levels'),
-              Item('colormap')),
-              buttons=["OK","Cancel"])
-    num_levels = Int(15)
-    colormap = Enum(color_map_name_dict.keys())
-    #---------------------------------------------------------------------------
-    # Private Traits
-    #---------------------------------------------------------------------------
-   
-    _image_index = Instance(GridDataSource)
-    _image_value = Instance(ImageData)
-    _cmap = Trait(jet, Callable)
-    #---------------------------------------------------------------------------
-    # Public View interface
-    #---------------------------------------------------------------------------
-    def __init__(self, *args, **kwargs):
-        super(PlotUI, self).__init__(*args, **kwargs)
-        self.create_plot()
-    def create_plot(self):
-        # Create the mapper, etc
-        self._image_index = GridDataSource(array([]), 
-                                          array([]), 
-                                          sort_order=("ascending","ascending"))
-        image_index_range = DataRange2D(self._image_index)
-        self._image_index.on_trait_change(self._metadata_changed, 
-                                          "metadata_changed")
-       
-        self._image_value = ImageData(data=array([]), value_depth=1)
-        image_value_range = DataRange1D(self._image_value)
-        # Create the contour plots
-        self.polyplot = ContourPolyPlot(index=self._image_index, 
-                                        value=self._image_value, 
-                                        index_mapper=GridMapper(range=
-                                            image_index_range), 
-                                        color_mapper=\
-                                            self._cmap(image_value_range),
-                                        levels=self.num_levels)
-        self.lineplot = ContourLinePlot(index=self._image_index, 
-                                        value=self._image_value, 
-                                        index_mapper=GridMapper(range=
-                                            self.polyplot.index_mapper.range),
-                                        levels=self.num_levels)
-        # Add a left axis to the plot
-        left = PlotAxis(orientation='left',
-                        title= "y",
-                        mapper=self.polyplot.index_mapper._ymapper,
-                        component=self.polyplot)
-        self.polyplot.overlays.append(left)
-   
-        # Add a bottom axis to the plot
-        bottom = PlotAxis(orientation='bottom',
-                          title= "x",
-                          mapper=self.polyplot.index_mapper._xmapper,
-                          component=self.polyplot)
-        self.polyplot.overlays.append(bottom)
-        # Add some tools to the plot
-        self.polyplot.tools.append(PanTool(self.polyplot, 
-                                           constrain_key="shift"))
-        self.polyplot.overlays.append(ZoomTool(component=self.polyplot, 
-                                            tool_mode="box", always_on=False))
-        self.polyplot.overlays.append(LineInspector(component=self.polyplot, 
-                                               axis='index_x',
-                                               inspect_mode="indexed",
-                                               write_metadata=True,
-                                               is_listener=False, 
-                                               color="white"))
-        self.polyplot.overlays.append(LineInspector(component=self.polyplot, 
-                                               axis='index_y',
-                                               inspect_mode="indexed",
-                                               write_metadata=True,
-                                               color="white",
-                                               is_listener=False))
-       
-        # Add these two plots to one container
-        contour_container = OverlayPlotContainer(padding=20,
-                                                 use_backbuffer=True, 
-                                                 unified_draw=True)
-        contour_container.add(self.polyplot)
-        contour_container.add(self.lineplot)
-        # Create a colorbar
-        cbar_index_mapper = LinearMapper(range=image_value_range)
-        self.colorbar = ColorBar(index_mapper=cbar_index_mapper,
-                                 plot=self.polyplot,
-                                 padding_top=self.polyplot.padding_top,
-                                 padding_bottom=self.polyplot.padding_bottom,
-                                 padding_right=40,
-                                 resizable='v',
-                                 width=30)
-        self.pd = ArrayPlotData(line_index = array([]),
-                                line_value = array([]),
-                                scatter_index = array([]),
-                                scatter_value = array([]),
-                                scatter_color = array([]))
-        self.cross_plot = Plot(self.pd, resizable="h")
-        self.cross_plot.height = 100
-        self.cross_plot.padding = 20
-        self.cross_plot.plot(("line_index", "line_value"), 
-                             line_style="dot")
-        self.cross_plot.plot(("scatter_index","scatter_value","scatter_color"),
-                             type="cmap_scatter",
-                             name="dot",
-                             color_mapper=self._cmap(image_value_range),
-                             marker="circle", 
-                             marker_size=8)
-        self.cross_plot.index_range = self.polyplot.index_range.x_range
-        self.pd.set_data("line_index2", array([])) 
-        self.pd.set_data("line_value2", array([])) 
-        self.pd.set_data("scatter_index2", array([])) 
-        self.pd.set_data("scatter_value2", array([])) 
-        self.pd.set_data("scatter_color2", array([])) 
-        self.cross_plot2 = Plot(self.pd, width = 140, orientation="v", resizable="v", padding=20, padding_bottom=160)
-        self.cross_plot2.plot(("line_index2", "line_value2"), 
-                             line_style="dot")
-        self.cross_plot2.plot(("scatter_index2","scatter_value2","scatter_color2"),
-                             type="cmap_scatter",
-                             name="dot",
-                             color_mapper=self._cmap(image_value_range),
-                             marker="circle", 
-                             marker_size=8)
-        self.cross_plot2.index_range = self.polyplot.index_range.y_range
-       
-        # Create a container and add components
-        self.container = HPlotContainer(padding=40, fill_padding=True,
-                                        bgcolor = "white", use_backbuffer=False)
-        inner_cont = VPlotContainer(padding=0, use_backbuffer=True)
-        inner_cont.add(self.cross_plot)
-        inner_cont.add(contour_container)
-        self.container.add(self.colorbar)
-        self.container.add(inner_cont)
-        self.container.add(self.cross_plot2)
-    def update(self, model):
-        self.minz = model.minz
-        self.maxz = model.maxz
-        self.colorbar.index_mapper.range.low = self.minz
-        self.colorbar.index_mapper.range.high = self.maxz
-        self._image_index.set_data(model.xs, model.ys)
-        self._image_value.data = model.zs
-        self.pd.set_data("line_index", model.xs)
-        self.pd.set_data("line_index2", model.ys)
-        self.container.invalidate_draw()
-        self.container.request_redraw()
- 
-    #---------------------------------------------------------------------------
-    # Event handlers
-    #---------------------------------------------------------------------------
-    def _metadata_changed(self, old, new):
-        """ This function takes out a cross section from the image data, based
-        on the line inspector selections, and updates the line and scatter
-        plots."""
-       
-        self.cross_plot.value_range.low = self.minz
-        self.cross_plot.value_range.high = self.maxz
-        self.cross_plot2.value_range.low = self.minz
-        self.cross_plot2.value_range.high = self.maxz
-        if self._image_index.metadata.has_key("selections"):
-            x_ndx, y_ndx = self._image_index.metadata["selections"]
-            if y_ndx and x_ndx:
-                self.pd.set_data("line_value", 
-                                 self._image_value.data[y_ndx,:])
-                self.pd.set_data("line_value2", 
-                                 self._image_value.data[:,x_ndx])
-                xdata, ydata = self._image_index.get_data()
-                xdata, ydata = xdata.get_data(), ydata.get_data()
-                self.pd.set_data("scatter_index", array([xdata[x_ndx]]))
-                self.pd.set_data("scatter_index2", array([ydata[y_ndx]]))
-                self.pd.set_data("scatter_value",
-                    array([self._image_value.data[y_ndx, x_ndx]]))
-                self.pd.set_data("scatter_value2",
-                    array([self._image_value.data[y_ndx, x_ndx]]))
-                self.pd.set_data("scatter_color",
-                    array([self._image_value.data[y_ndx, x_ndx]]))
-                self.pd.set_data("scatter_color2",
-                    array([self._image_value.data[y_ndx, x_ndx]]))
-        else:
-            self.pd.set_data("scatter_value", array([]))
-            self.pd.set_data("scatter_value2", array([]))
-            self.pd.set_data("line_value", array([]))
-            self.pd.set_data("line_value2", array([]))
-    def _colormap_changed(self):
-        self._cmap = color_map_name_dict[self.colormap]
-        if hasattr(self, "polyplot"):
-            value_range = self.polyplot.color_mapper.range
-            self.polyplot.color_mapper = self._cmap(value_range)
-            value_range = self.cross_plot.color_mapper.range
-            self.cross_plot.color_mapper = self._cmap(value_range)
-            # FIXME: change when we decide how best to update plots using
-            # the shared colormap in plot object
-            self.cross_plot.plots["dot"][0].color_mapper = self._cmap(value_range)
-            self.cross_plot2.plots["dot"][0].color_mapper = self._cmap(value_range)
-            self.container.request_redraw()
-    def _num_levels_changed(self):
-        if self.num_levels > 3:
-            self.polyplot.levels = self.num_levels
-            self.lineplot.levels = self.num_levels
-
 
 class CaptureThread(Thread):
     def run(self):
@@ -268,42 +65,15 @@ class CaptureThread(Thread):
             for j in xrange(99,-1,-1):
                 self.fd.intensity_map[i,j] = power_meter.getPower()
                 stage.right(1,self.step_amplitude)
-            stage.up(1,self.step_amplitude)
+            #stage.up(1,self.step_amplitude)
+            stage.forwards(1, self.step_amplitude)
         print self.fd.intensity_map
 
 class CustomTool(BaseTool): 
 
     #right click
     def normal_right_down(self, event):
-        dialog = FileDialog(action="save as", wildcard=self.file_wildcard)
-        dialog.open()
-        if dialog.return_code == OK:
-            self.filedir = dialog.directory
-            self.filename = dialog.filename
-            self._save_to_file()
-
-class ModelView(HasTraits):
-   
-    model = Instance(Model)
-    view = Instance(PlotUI)
-    
-    traits_view = View(Item('@view', 
-                            show_label=False),
-                       menubar=MenuBar(Menu(Action(name="Edit Model",
-                                                   action="edit_model"),
-                                            Action(name="Edit Plot",
-                                                   action="edit_plot"),
-                                            CloseAction,
-                                            name="File")),
-                       handler = Controller,
-                       title = "Function Inspector", 
-                       resizable=True)
-   
-    @on_trait_change('model, view')
-    def update_view(self):
-        if self.model is not None and self.view is not None:
-            self.view.update(self.model)
-
+        print event
 
 class FieldDataController(HasTraits):
     
@@ -319,17 +89,27 @@ class FieldDataController(HasTraits):
     capture_thread=Instance(CaptureThread) 
     label_button_measurment = Str('Start acquisition')
     
-    test = Button
-    
-    file_name= File
-    file_name = open_file()
-
+    _save_file = File
     # Define the view associated with this controller:
     view = View(Item('thread_control' , label="Acquisition", editor = ButtonEditor(label_value = 'label_button_measurment')),
                 'step_amplitude',
                 Item('plot',editor=ComponentEditor(),show_label=False),
-                'test',
+                menubar=MenuBar(
+                   Menu(Action(name="Save Plot", action="save"), # see Controller for
+                        Action(name="Load Plot", action="load"), # these callbacks
+                        Separator(),
+                        CloseAction,
+                        name="File")),
                 resizable=True)
+    
+    save_file_view = View(
+        Item('_save_file'), 
+        buttons=OKCancelButtons, 
+        kind='livemodal',  # NB must use livemodal, plot objects don't copy well
+        width=400,
+        resizable=True,
+    )
+            
     
     def __init__(self,*args, **kw):
         super(FieldDataController, self).__init__(*args, **kw)
@@ -384,9 +164,6 @@ class FieldDataController(HasTraits):
             self.label_button_measurment = 'Stop acquisition'
             #import time
             #time.sleep(0.1)
-    def _test_fired(self):
-        self.model.intensity_map = ones((100,100))
-        print 'changed something'
             
     def _step_amplitude_changed(self):
         if self.capture_thread:
@@ -394,16 +171,18 @@ class FieldDataController(HasTraits):
             
     @on_trait_change('model.intensity_map')        
     def updatePlot(self,object,name,old,new):
-        print 'yeah somebody called me'
         if self.plot_data:
             self.plot_data.set_data('imagedata',object.intensity_map)
+
+    def save(self):
+        """
+        Callback for the 'Save Image' menu option.
+        """
+        ui = self.edit_traits(view='save_file_view')
+        if ui.result == True:
+            save(self._save_file, self.model.intensity_map)
             
-    def mousedown_left(self, event):
-        print 'rightclick'
-        self.event_state = "mousedown"
-        event.handled = True
         
     
 ui = FieldDataController(model=FieldData(index=array([]),data=array([]), intensity_map=array([[0][0]])))
-ui.edit_traits(kind="livemodal")
-    
+ui.configure_traits(view='view')

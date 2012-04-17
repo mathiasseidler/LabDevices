36026a550ca8c6ce54062325c946db90e926cafe
Mathias S <mseidler.work@gmail.com>
2012-04-18 00:36:56 +0800

    minor changes in mayavi

--- Mayavi.py
+++ Mayavi.py
@@ -3,7 +3,7 @@
 # License: BSD Style.
 
 # Standard imports.
-from numpy import sqrt, sin, mgrid
+from numpy import sqrt, sin, mgrid, ogrid
 
 # Enthought imports.
 from traits.api import HasTraits, Instance, Property, Enum
@@ -11,6 +11,7 @@ from traitsui.api import View, Item, HSplit, VSplit, InstanceEditor
 from tvtk.pyface.scene_editor import SceneEditor
 from mayavi.core.ui.engine_view import EngineView
 from mayavi.tools.mlab_scene_model import MlabSceneModel
+from mayavi import mlab
 
 class Mayavi(HasTraits):
 
@@ -49,13 +50,24 @@ class Mayavi(HasTraits):
 
     def generate_data_mayavi(self):
         """Shows how you can generate data using mayavi instead of mlab."""
-        from mayavi.sources.api import ParametricSurface
-        from mayavi.modules.api import Outline, Surface
+        #from mayavi.sources.api import ParametricSurface
+        #from mayavi.modules.api import Outline, Surface
+        x, y, z = ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
+        s = sin(x*y*z)/(x*y*z)
         e = self.scene.engine
-        s = ParametricSurface()
-        e.add_source(s)
-        e.add_module(Outline())
-        e.add_module(Surface())
+        #s = ParametricSurface()
+        #e.add_source(s)
+        #e.add_module(Outline())
+        #e.add_module(Surface())
+        #mlab.pipeline.volume(mlab.pipeline.scalar_field(s), vmin=0.2, vmax=0.8)
+        
+        src = mlab.pipeline.scalar_field(s)
+        mlab.pipeline.iso_surface(src, contours=[s.min()+0.1*s.ptp(), ], opacity=0.1)
+        mlab.pipeline.iso_surface(src, contours=[s.max()-0.1*s.ptp(), ],)
+        mlab.pipeline.image_plane_widget(src,
+                            plane_orientation='z_axes',
+                            slice_index=10,
+                        )
 
     def _selection_change(self, old, new):
         self.trait_property_changed('current_selection', old, new)
